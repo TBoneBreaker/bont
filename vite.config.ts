1e8f346df871eb797bcbeb5cae4cfad92568de41
TBoneBreaker <thomas.steinert.05@outlook.de>
2026-09-05 23:43:57 +0200

    Use lightweight SVG app icon

--- vite.config.ts
+++ vite.config.ts
@@ -18,8 +18,7 @@ export default defineConfig({
         orientation: 'portrait',
         start_url: '/',
         icons: [
-          { src: '/pwa-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/pwa-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+          { src: '/favicon.svg', sizes: 'any', type: 'image/svg+xml', purpose: 'any maskable' }
         ]
       },
       workbox: {
